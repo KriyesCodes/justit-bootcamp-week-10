569753a9b749955e3b0b8ab79b51c953cc6d1cc9
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-20 15:41:58 +0000

    Update userInput.py

--- Pt7_FilesDictsCodeBase/1_FileHandling/5_userInput.py
+++ Pt7_FilesDictsCodeBase/1_FileHandling/5_userInput.py
@@ -6,7 +6,17 @@ age      =    int(input("Enter your age: "))
 "Make"
 "To Do: Task 1: Use the code above to ask for user input and then save it to a file called userDetails.txt"
 
+with open('Pt7_FilesDictsCodeBase/1_FileHandling/userDetails.txt', 'a') as userData:
+  "method 1"
+  # userData.write(fname + " " + address + " " + interest + " " + str(age))
 
+  "method 2"
+  # userData.write(f"\n{fname} {address} {interest} {age}")
+  
+  "method 3"
+  someData = f"\n{fname} {address} {interest} {age}"
+  userData.write(someData)
+  
 
 
 "Further reading"
