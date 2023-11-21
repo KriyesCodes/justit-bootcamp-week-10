413c89b6eb8fea0a0e461d9b25928e32329fb23d
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-21 13:26:47 +0000

    Add ticketing.py with TODOs for the project

---
+++ ticketing-program-project/ticketing.py
@@ -0,0 +1,12 @@
+# TODO: Provide a welcome message
+# TODO: Display the entrance ticket prices
+# TODO: Ask how many adult tickets are required
+# TODO: Ask how many child tickets are required
+# TODO: Ask how many senior citizen tickets are required
+# TODO: Ask for the lead booker surname (for the ticket)
+# TODO: Ask if they require a parking pass for the car park
+# TODO: Add the total number of tickets to display the total cost
+# TODO: Print a ticket (display lead booker surname, tickets purchased, today's date)
+#       Print a car parking pass (if requested)
+#       Use data validation techniques to avoid runtime errors through incorrect data entry
+#       Thank the customer for their purchase
