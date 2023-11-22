54055c112b1305fd22dff29301f17a28feb32f3d
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-22 14:11:43 +0000

    Add tickets input with exception handling

--- ticketing-program-project/ticketing.py
+++ ticketing-program-project/ticketing.py
@@ -1,9 +1,9 @@
-# TODO: Provide a welcome message
-# TODO: Display the entrance ticket prices
-# TODO: Ask how many adult tickets are required
-# TODO: Ask how many child tickets are required
-# TODO: Ask how many senior citizen tickets are required
-# TODO: Ask for the lead booker surname (for the ticket)
+# TODO: Provide a welcome message Y
+# TODO: Display the entrance ticket prices Y
+# TODO: Ask how many adult tickets are required Y
+# TODO: Ask how many child tickets are required Y
+# TODO: Ask how many senior citizen tickets are required Y
+# TODO: Ask for the lead booker surname (for the ticket) Y
 # TODO: Ask if they require a parking pass for the car park
 # TODO: Add the total number of tickets to display the total cost
 # TODO: Print a ticket (display lead booker surname, tickets purchased, today's date)
@@ -23,5 +23,28 @@ def printTicketPrices():
   print ("{:<20} {:>10}".format('Child','£12'))
   print ("{:<20} {:>10}".format('Senior Citizen','£11'))
 
+def getTicket(prompt):
+  try:
+    tickets = int(input(prompt))
+    if (tickets < 0):
+      return Exception
+  except:
+    return Exception
+  
+  return tickets
+
 printWelcomeMessage()
 printTicketPrices()
+
+transactionCompleted = False
+
+while not(transactionCompleted):
+  try:
+    adultTickets = getTicket("How many adult tickets would you like? ")
+    childTickets = getTicket("How many child tickets would you like? ")
+    seniorTickets = getTicket("How many senior citizen tickets would you like? ")
+  except(Exception):
+    print("Please enter a non-negative integer")
+    continue
+
+
