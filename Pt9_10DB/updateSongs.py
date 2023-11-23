5cb14ddd60b0ac743ef97ca72d5130b404e27d52
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-23 21:45:58 +0000

    Add updateSongs.py

--- Pt9_10DB/updateSongs.py
+++ Pt9_10DB/updateSongs.py
@@ -3,16 +3,27 @@ from connect import *
 def update_songs():
   # use primary key to update a record
 
-  idField = input("Enter the song ID of the record you want to update: ")
+  idField = int(input("Enter the song ID of the record you want to update: "))
 
   # field to update
   fieldName = input("Enter Title or Artist or Genre as field name: ").title()
 
   fieldValue = input(f"Enter the value for {fieldName} field: ")
 
-  dbCursor.execute(f"UPDATE songs SET {fieldName} = '{fieldValue}' WHERE SongID = {idField}")
+  if (fieldName == 'Title'):
+    query = "UPDATE songs SET Title = ? WHERE SongID = ?"
+  elif (fieldName == 'Artist'):
+    query = "UPDATE songs SET Artist = ? WHERE SongID = ?"
+  elif (fieldName == 'Genre'):
+    query = "UPDATE songs SET Genre = ? WHERE SongID = ?"
+  else:
+    print("Incorrect input")
+    return
+  
+  dbCursor.execute(query, (fieldValue, idField))
 
   dbCon.commit()
+  
   print(f"Record {idField} updated")
 
 if __name__ == "__main__":
